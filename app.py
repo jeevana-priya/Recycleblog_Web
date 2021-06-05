76170a8a4d0118cb39788eb4e8db9ff55ab405be
Jeevana-priya <jeevan09priya@gmail.com>
2021-06-05 22:06:26 +0530

    my first commit

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ app=Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+
+@app.route('/priya')
+def priya():
+    return "wrgrgr"
+
+
+# @app.route('/')
+# def homepage():
+#     return render_template('index.html')
+
+
+# @app.route('/')
+# def homepage():
+#     return render_template('index.html')
+
+
+# @app.route('/')
+# def homepage():
+#     return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True)
 
